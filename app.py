18d1a8be723bae1c3943c95cbfe1aaa6738ee978
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-01-26 14:11:31 +0100

    create index.html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 
 app = Flask(__name__)
 
-
+"""
+[
+    {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
+    {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
+    # More blog posts can go here...
+]
+"""
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    with open('data/data.json', 'r') as f:
+        blog_posts = json.load(f)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
